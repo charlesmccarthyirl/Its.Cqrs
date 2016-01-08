30088a037465e962b4d8b266880b1d50df6b021a
unknown <chmccart@CHMCCART2.redmond.corp.microsoft.com>
2016-01-08 13:43:55 -0800

    RemovingScheduledCommand<TAggregate> constraint of TAggregate being IEventSourced. Adding tests for ApplyToAsync of non aggregate commands

--- Domain.Tests/NonAggregateCommandTests.cs
+++ Domain.Tests/NonAggregateCommandTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.Its.Recipes;
 using NUnit.Framework;
 
 namespace Microsoft.Its.Domain.Tests
@@ -7,16 +9,61 @@ namespace Microsoft.Its.Domain.Tests
     [TestFixture]
     public class NonAggregateCommandTests
     {
+        internal static int CallCount;
+
+        [SetUp]
+        public void Setup()
+        {
+            CallCount = 0;
+        }
+
         [Test]
         public void non_event_sourced_command_schedulers_can_be_resolved()
         {
-            Action getScheduler = () =>  Configuration.Current.CommandScheduler<Foo>();
+            Action getScheduler = () => Configuration.Current.CommandScheduler<Foo>();
 
             getScheduler.ShouldNotThrow();
+        }
+
+        [Test]
+        public async Task when_a_command_is_applied_directly_the_command_is_executed()
+        {
+            Command<Foo>.AuthorizeDefault = (account, command) => true;
+            var validationString = Any.String();
+            var doStuff = new DoStuff { Validation = validationString };
+            await doStuff.ApplyToAsync(new Foo());
+            CallCount.Should().Be(1);
+        }
+
+        [Test]
+        public async Task when_a_command_is_applied_directly_with_an_etag_the_command_is_executed()
+        {
+            Command<Foo>.AuthorizeDefault = (account, command) => true;
+            var validationString = Any.String();
+            var doStuff = new DoStuff { Validation = validationString, ETag = Any.Guid().ToString() };
+            await doStuff.ApplyToAsync(new Foo());
+            CallCount.Should().Be(1);
         }
     }
 
     public class Foo
     {
+    }
+
+    public class DoStuffCommandHandler : ICommandHandler<Foo, DoStuff>
+    {
+        public async Task EnactCommand(Foo aggregate, DoStuff command)
+        {
+            NonAggregateCommandTests.CallCount++;
+        }
+
+        public async Task HandleScheduledCommandException(Foo aggregate, CommandFailed<DoStuff> command)
+        {
+        }
+    }
+
+    public class DoStuff : Command<Foo>
+    {
+        public string Validation { get; set; }
     }
 }
